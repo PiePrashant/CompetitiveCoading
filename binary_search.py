5142137e2ae1c1e0593c6592aee76c9818e8fcf3
prashant <prashant@iitkgp.ac.in>
2018-08-18 19:01:26 +0530

    takes 3 arguments array, key and 0

---
+++ binary_search.py
@@ -0,0 +1,10 @@
+def binary_search(a, k,i):
+    n = len(a)
+    if a[int(n/2)] == k: return i + int(n/2)
+    elif n == 1: return "OOPS! NOT FOUND"
+    elif a[int(n/2)] > k: return binary_search(a[0:int(n/2)], k, i)
+    else: return binary_search(a[int(n/2):n], k, i + int(n/2))
+
+
+
+print(binary_search([1],1,0))
